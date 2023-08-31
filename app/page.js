1e771050311729e658028e056c669f3c290cb0cb
Damian Wrather <damian.wrather@gmail.com>
2023-08-30 20:26:06 -0500

    home page

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, {useState} from 'react'
 import Image from "next/image";
 import desktop1 from '../public/images/desktop-image-hero-1.jpg'
 import logo from '../public/images/logo.svg'
@@ -10,12 +11,26 @@ import about_light from '../public/images/image-about-light.jpg'
 
 
 export default function Home() {
+  let data = [{img:'/images/desktop-image-hero-1.jpg', header: 'Discover innovative ways to decorate', paragraph:'We provide unmatched quality, comfort, and style for property owners across the country.\n' +
+        '                Our experts combine form and function in bringing your vision to life. Create a room in your\n' +
+        '                own style with our collection and make your property a reflection of you and what you love.'},{img:'/images/desktop-image-hero-2.jpg', header: ' We are available all across the globe', paragraph:'With stores all over the world, it\'s easy for you to find furniture for your home or place of business. \n' +
+        '  Locally, we’re in most major cities throughout the country. Find the branch nearest you using our \n' +
+        '  store locator. Any questions? Don\'t hesitate to contact us today.'},{img:'/images/desktop-image-hero-3.jpg', header: 'Manufactured with the best materials', paragraph:' Our modern furniture store provide a high level of quality. Our company has invested in advanced technology \n' +
+        '  to ensure that every product is made as perfect and as consistent as possible. With three decades of \n' +
+        '  experience in this industry, we understand what customers want for their home and office.'}]
+  const [info, setInfo] = useState();
+  let image = data[0].img
+  let header = data[0].header
+  let paragraph = data[0].paragraph
+  function handleClick(){
+    setInfo()
+  }
   return (
     <>
       <div className="container">
         <div className="row no-gap no-padding full-width">
           <div className="column header-image">
-            <Image src="/images/desktop-image-hero-1.jpg" alt="chair" className="header-img" width={1000} height={600}/>
+            <Image src={image} alt="chair" className="header-img" width={1000} height={600}/>
               <div className="logo-wrapper">
                 <Image src={logo} alt="room logo"/>
               </div>
@@ -34,14 +49,10 @@ export default function Home() {
             <div className="column justify-center align-center header-padding">
               <div className="column justify-center align-center shrink">
                 <div className="row shrink header-row">
-                  <h1 className="header-text">Discover innovative ways to decorate</h1>
+                  <h1 className="header-text">{header}</h1>
                 </div>
                 <div className="row shrink paragraph-row">
-                  <p className="paragraph-text">
-                    We provide unmatched quality, comfort, and style for property owners across the country.
-                    Our experts combine form and function in bringing your vision to life. Create a room in your
-                    own style with our collection and make your property a reflection of you and what you love.
-                  </p>
+                  <p className="paragraph-text">{paragraph}</p>
                 </div>
               </div>
               <div className="row">
@@ -54,7 +65,7 @@ export default function Home() {
             <div className="row arrow-row arrow-box gap-30">
               <div className="column justify-center shrink">
                 <div className="arrow-wrapper">
-                  <Image src={left_arrow} alt="arrow" className="left-arrow"/>
+                  <Image src={left_arrow} alt="arrow" className="left-arrow" onClick={handleClick}/>
                 </div>
               </div>
               <div className="column justify-center shrink">
